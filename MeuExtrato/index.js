14cbfff4fd1e1c353213f0561a55c68d7670fbd2
Fabiana Oliveira <fabi.ana_deoliveira@outlook.com>
2021-12-10 04:17:34 -0300

    layout adjustments

--- MeuExtrato/index.js
+++ MeuExtrato/index.js
@@ -1,3 +1,8 @@
+
+var card = document.querySelector('.card');
+card.addEventListener( 'click', function() {
+  card.classList.toggle('is-flipped');
+});
 const signInBtn = document.getElementById("signIn");
 const signUpBtn = document.getElementById("signUp");
 const fistForm = document.getElementById("form1");
@@ -5,12 +10,14 @@ const secondForm = document.getElementById("form2");
 const container = document.querySelector(".container");
 
 signInBtn.addEventListener("click", () => {
-	container.classList.remove("right-panel-active");
+	container.classList.remove("flip-card-front");
 });
 
 signUpBtn.addEventListener("click", () => {
-	container.classList.add("right-panel-active");
+	container.classList.add("flip-card-back");
 });
 
 fistForm.addEventListener("submit", (e) => e.preventDefault());
 secondForm.addEventListener("submit", (e) => e.preventDefault());
+
+
