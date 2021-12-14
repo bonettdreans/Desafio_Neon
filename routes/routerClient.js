96ae1727a28a2318b8ae3d8821a3534f24f184eb
guzmandp <guzmandp@gmail.com>
2021-12-14 00:52:41 -0300

    clearing code
    updating requisitions

--- routes/routerClient.js
+++ routes/routerClient.js
@@ -4,14 +4,6 @@ const clientControllers = require('../app/controllers/clientControllers');
 
 const routerClient = express.Router();
 
-routerClient.get("/clients", clientControllers.findAll);
-
-routerClient.get("/client/:id", clientControllers.findOne);
-
-routerClient.post("/client/Create", clientControllers.createUser);
-
-routerClient.put("/client/:id", clientControllers.updateUser);
-
-routerClient.delete("/client/:id", clientControllers.deletUser);
+routerClient.get("/api/client/:id", clientControllers.findOne);
 
 module.exports = routerClient
--- routes/routesLaunch.js
+++ routes/routesLaunch.js
@@ -4,18 +4,12 @@ const launchControllers = require("../app/controllers/launchControllers")
 
 const routesLaunch = express.Router();
 
+routesLaunch.get("/api/launch/:id", launchControllers.clientLaunch);
 
+routesLaunch.post("/api/lauch/", launchControllers.createLaunch);
 
+routesLaunch.put("/api/launch/:id", launchControllers.updateLaunch);
 
-
-routesLaunch.get("/launchs", launchControllers.findAll);
-
-routesLaunch.get("/launch/:id", launchControllers.findOne);
-
-routesLaunch.post("/launch/create", launchControllers.createLaunch);
-
-routesLaunch.put("/launch/:id", launchControllers.updateLaunch);
-
-routesLaunch.delete("/launch/:id", launchControllers.deletLaunch);
+routesLaunch.delete("/api/launch/:id", launchControllers.deletLaunch);
 
 module.exports = routesLaunch
