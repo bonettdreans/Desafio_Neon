29efc60ac3415d0c94855da7af8e3d16633869d1
Hector Bonett <hgilbonett19@gmail.com>
2021-12-15 12:13:51 -0400

    Adding updates to deploy to heroku

--- config/database.js
+++ config/database.js
@@ -1,10 +1,10 @@
 require('dotenv').config()
 
 module.exports = {
-  username: process.env.DB_USERNAME || "root",
-  password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_DATABASE || "Neon2",
-  host: process.env.DB_HOST ||"localhost",
+  username: process.env.DB_USERNAME || "rqloml7vew3bmz23",
+  password: process.env.DB_PASSWORD || "o75zme2kqwdlfqze",
+  database: process.env.DB_DATABASE || "vv1spcordp4sflqt",
+  host: process.env.DB_HOST ||"uzb4o9e2oe257glt.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   dialect:process.env.DB_DIALECT ||"mysql",
   define: {
     timestamps: false, //Apaga arquivos Crea
