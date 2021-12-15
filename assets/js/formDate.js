12ec6bfb82ea1fe5647683ebd4e053f1bbaee9b1
Hector Bonett <hgilbonett19@gmail.com>
2021-12-15 11:51:38 -0400

    Preparing Branch for production

--- assets/js/formDate.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("formDate").onsubmit = function() {
-    let formData = document.getElementById("formDate").elements;
-    document.getElementById("resultado").innerHTML = formData["data"].value;
-    return false;
-}
--- page-launch/js/script.js
+++
@@ -1,5 +0,0 @@
-
-/* Formatting the value field */
-document.getElementById("txtValue").addEventListener("change", function(){
-    this.value = parseFloat(this.value).toFixed(2);
-});
