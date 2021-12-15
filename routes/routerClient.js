8f811a5848b47a1e204e817ea3a962529360dd27
Marcos Cucoro <marcos.cucoro@hotmail.com>
2021-12-14 21:46:30 -0300

    conflicts correction

--- routes/routerClient.js
+++ routes/routerClient.js
@@ -1,26 +1,15 @@
 const express = require('express');
 
 const clientControllers = require('../app/controllers/clientControllers');
-<<<<<<< HEAD
-
-=======
->>>>>>> 429eff1b8e5f01ca6f06c94267c168d5d1384b5e
 
 const auth = require ('../app/middlewares/auth')
 
 const routerClient = express.Router();
 
 routerClient.get("/api/client/:id", auth, clientControllers.findOne);
-<<<<<<< HEAD
-
-=======
->>>>>>> 429eff1b8e5f01ca6f06c94267c168d5d1384b5e
 
 
-<<<<<<< HEAD
 routerClient.get("/api/client/:id", clientControllers.findOne);
 
-=======
->>>>>>> 429eff1b8e5f01ca6f06c94267c168d5d1384b5e
 
 module.exports = routerClient
