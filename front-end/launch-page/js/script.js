d2772cfe3c1dab45d3396808870dc4602bc119b3
Marcos Cucoro <marcos.cucoro@hotmail.com>
2021-12-17 17:31:06 -0300

    Delete unused js file, hide balance button placed

--- front-end/launch-page/js/script.js
+++ front-end/launch-page/js/script.js
@@ -1,51 +1,19 @@
 document.getElementById("txtValue").addEventListener("change", function () {
     this.value = parseFloat(this.value).toFixed(2);
 });
-
-var inputs = $('[type="checkbox"]');
-inputs.on('click', function () {
-    inputs.get().forEach(function (el) {
-        el.checked = el == this && this.checked;
-    }, this);
-});
-
-let url = `https://app-asuma.herokuapp.com/`
-const postLaunch = (launch) => {
-    fetch(`${url}/api/launch/`)
-        .then(x => x.text())
-        .then(JSON.parse)
-};
-const getValue = () => {
-    const value = '';
-    console.log(value);
-};
-const postForm = () => {
-    let field = document.getElementById("txtValue").value;
-    if (field.match(/^[0-9]{8}$/)) {
-        searchCep(field);
-        field = "";
+function mostrarOcultar() {
+    let saldo = document.getElementById("saldo");
+    let btn = document.getElementById('bma');
+    let olho = document.getElementById('olhoaberto');
+    if (saldo.type == "text") {
+        console.log(saldo.type);
+        console.log(btn.id);
+        saldo.type = "password";
+        olho.src = '../front-end/assets/icons/eye-slash.svg';
+    } else if (saldo.type == "password") {
+        console.log(saldo.type);
+        console.log(btn.id);
+        saldo.type = "text";
+        olho.src = '../front-end/assets/icons/eye.svg'; 
     }
 }
-console.log(postForm())
-
-const desRec = document.getElementById("btn1");
-const txtValue = document.getElementById("txtValue");
-const txtDate = document.getElementById("txtDate");
-const txtDeposit = document.getElementById("txtDeposit");
-const select = document.getElementById("selected");
-const errorValue = document.getElementById("errorValue");
-const errorDate = document.getElementById("errorDate");
-const errorDeposit = document.getElementById("errorDeposit");
-
-form.addEventListener("submit", e => {
-    e.preventDefault()
-
-    const msgError = [];
-    if (txtValue.value === null || txtValue.value === "") {
-        txtValue.classList.add("errorValue")
-        errorValue.innerHTML = `</p>Campo obrigatório`;
-    } else {
-        errorValue.classList.remove("errorInput")
-    }
-
-})
