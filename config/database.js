6709adcfee3c12f2e9a2c58de713ffa755bf35a6
Hector Bonett <hgilbonett19@gmail.com>
2021-12-19 12:36:01 -0400

    updating access credentials

--- config/database.js
+++ config/database.js
@@ -1,10 +1,10 @@
 require('dotenv').config()
 
 module.exports = {
-  username: process.env.DB_USERNAME || "rqloml7vew3bmz23",
-  password: process.env.DB_PASSWORD || "o75zme2kqwdlfqze",
-  database: process.env.DB_DATABASE || "vv1spcordp4sflqt",
-  host: process.env.DB_HOST ||"uzb4o9e2oe257glt.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  username: process.env.DB_USERNAME || "root",
+  password: process.env.DB_PASSWORD || "",
+  database: process.env.DB_DATABASE || "secontrole",
+  host: process.env.DB_HOST ||"localhost",
   dialect:process.env.DB_DIALECT ||"mysql",
   define: {
     timestamps: false, //Apaga arquivos Crea
