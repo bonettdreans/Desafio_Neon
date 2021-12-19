1b8ba613eb035ccc27deee6f091e07ac1235fa40
Marcos Cucoro <marcos.cucoro@hotmail.com>
2021-12-19 19:22:27 -0300

    extract page changes

---
+++ front-end/assets/js/extract.js
@@ -0,0 +1,11 @@
+function edit() {
+    alert("Aguarde a próxima versão");
+};
+
+function delet() {
+    alert("Aguarde a próxima versão")
+};
+
+function perfil() {
+    alert("Aguarde a próxima versão")
+};
