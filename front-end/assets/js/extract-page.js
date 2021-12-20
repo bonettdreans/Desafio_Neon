b089b2bee3e51db2da8359ca1be168baf7829ef0
Isabel Lacerda <contato.lacerdaisab@outlook.com>
2021-12-20 16:07:36 -0300

    updating extract page js

--- front-end/assets/js/extract-page.js
+++ front-end/assets/js/extract-page.js
@@ -1,30 +1,31 @@
-const options = {
-  method: "GET",
-  mode: "cors",
-  cache: "default",
-};
-const id = getUrl.get('id')
-const url = `http://localhost:8000/client/${id}`
+const dataLS = JSON.parse(localStorage.getItem('client'));
+const token = localStorage.getItem('token')
+const id = dataLS.user.client_id
+console.log("Perfect")
 
-console.log(`${url}/${id}`)
-  fetch(`${url}/${id}`, options)
-    .then(response => response.json())
-    .then(data => mostrarData(data))
-    .catch((error) => console.log("Erro:" + error))
-const mostrarData = (data) => {
-  let dataComplete = data.data.valor;
-    console.log(data.data.valor)
+fetch(`http://localhost:8000/api/client/${id}/launch/`, {
+      method: 'GET',
+      mode: 'cors',
+      cache: 'default',
+      headers: {
+        'Content-Type': 'application/json',
+        'authorization': `Bearer ${token}`,
+      },
+    }).then(response => response.json())
+      .then(data => mostrarData(data))
+      .catch((error) => console.log("Erro:" + error))
+        const mostrarData = (data) => {
+  let dataComplete = data.data.valor[0];
+    //console.log(data.data.valor[0])
   let body = ''
         for (let i = 0; i < dataComplete.length; i++){
-      body += `<tr><td/>${dataComplete[i].title_launch}<td/>${dataComplete[i].amount}<td/>${dataComplete[i].date}<td/>Edit    Delete <tr>`;     
-        }  
-  document.getElementById("data").innerHTML = body;
-  let nomeUsuario = ''
-  nomeUsuario += `${data.data.name}`
-  document.getElementById("nomePerfil").innerHTML = nomeUsuario;
-
-  var balance = data.data.valor;
+      body += `<tr><td/>${dataComplete[i].date}<td/>${dataComplete[i].type_launch}<td/>${dataComplete[i].title_launch}<td/>${dataComplete[i].amount}`;     
+        
+        }
+          document.getElementById("data").innerHTML = body;
+         document.getElementById("data").classList.add("columnsneg")
+  var balance = data.data.valor[0];
   var totalamount = balance.reduce((sum, value) => (typeof value.amount == "number" ? sum + value.amount : sum), 0);
-  console.log(totalamount);
-  document.getElementById("saldo").innerHTML = "R$  " + totalamount ;
+          document.getElementById("saldo").innerHTML = "R$  " + totalamount;
+          console.log(totalamount)
 };
