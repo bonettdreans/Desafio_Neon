6582ffae4ca44d2522402f0af5f92909698ec43d
guzmandp <guzmandp@gmail.com>
2021-12-20 15:24:24 -0300

    update frontEnd

--- front-end/assets/js/login.js
+++ front-end/assets/js/login.js
@@ -1,42 +1,19 @@
 console.clear();
 
-const loginBtn = document.getElementById('login');
-const signupBtn = document.getElementById('signup');
-
-loginBtn.addEventListener('click', (e) => {
-	let parent = e.target.parentNode.parentNode;
-	Array.from(e.target.parentNode.parentNode.classList).find((element) => {
-		if(element !== "slide-up") {
-			parent.classList.add('slide-up')
-		}else{
-			signupBtn.parentNode.classList.add('slide-up')
-			parent.classList.remove('slide-up')
-		}
-	});
-});
-
-signupBtn.addEventListener('click', (e) => {
-	let parent = e.target.parentNode;
-	Array.from(e.target.parentNode.classList).find((element) => {
-		if(element !== "slide-up") {
-			parent.classList.add('slide-up')
-		}else{
-			loginBtn.parentNode.parentNode.classList.add('slide-up')
-			parent.classList.remove('slide-up')
-		}
-	});
-});
-
 const password = document.getElementById("password");
 const email = document.getElementById("email");
 const form = document.getElementById("form_login")
 const warningsEmailLogin = document.getElementById("warningsEmailLogin");
 const warningsPasswordLogin = document.getElementById("warningsPasswordLogin");
 
-form.addEventListener("submit", e => { e.preventDefault()
+form.addEventListener("submit", (event) => {
+  validation(event)
+})
+
+async function validation(event){
 
 const expresion = /\w+@\w+\.+[a-z]/;
-const msgErro = [];
+
   if (password.value === null || password.value === "") {
     password.classList.add("warningsInput")
     warningsPasswordLogin.innerHTML = `<p/>⚠️ Senha não pode ser vazia`;
@@ -67,8 +44,10 @@ const msgErro = [];
     warningsEmailLogin.innerHTML = ""
     email.classList.remove("warningsInput")
   }
-  //document.getElementById("form_login").reset()
-})  
+ await envioLogin(event)
+  document.getElementById("form_login").reset()
+
+}
 const nameCadastro = document.getElementById("nameCadastro")
 const passwordCadastro = document.getElementById("passwordCadastro");
 const repPasswordCadastro = document.getElementById("repPasswordCadastro")
@@ -78,8 +57,15 @@ const warningsNameCadastro = document.getElementById("warningsNameCadastro");
 const warningsEmailCadastro = document.getElementById("warningsEmailCadastro");
 const warningsPasswordCadastro = document.getElementById("warningsPasswordCadastro");
 
-formCadastro.addEventListener("submit", e => {
-  e.preventDefault()
+
+
+formCadastro.addEventListener("submit", (event) => {
+  validarCadastro(event)
+})
+
+
+async function validarCadastro(event){
+  
 const expresion = /\w+@\w+\.+[a-z]/;
   const msgErro = [];
   if (nameCadastro.value === null || nameCadastro.value === "") {
@@ -96,36 +82,36 @@ const expresion = /\w+@\w+\.+[a-z]/;
     warningsPasswordCadastro.innerHTML = ""
     passwordCadastro.classList.remove("warningsInput")
   }
-  // if (passwordCadastro.value.length < 6 || passwordCadastro.value.length > 18) {
-  //   passwordCadastro.classList.add("warningsInput")
-  //   warningsPasswordCadastro.innerHTML = `⚠️ Senha precisa ter no mínimo 6 e no máximo 10 carácteres`;
-  // } else {
-  //   warningsPasswordCadastro.innerHTML = ""
-  //   passwordCadastro.classList.remove("warningsInput")
-  // }
-  // if (repPasswordCadastro.value === null || repPasswordCadastro.value === "") {
-  //   repPasswordCadastro.classList.add("warningsInput")
-  //   warningsPasswordCadastro.innerHTML = `⚠️Senha não pode ser vazia`;
+  if (passwordCadastro.value.length < 6 || passwordCadastro.value.length > 18) {
+    passwordCadastro.classList.add("warningsInput")
+    warningsPasswordCadastro.innerHTML = `⚠️ Senha precisa ter no mínimo 6 e no máximo 10 carácteres`;
+  } else {
+    warningsPasswordCadastro.innerHTML = ""
+    passwordCadastro.classList.remove("warningsInput")
+  }
+  if (repPasswordCadastro.value === null || repPasswordCadastro.value === "") {
+    repPasswordCadastro.classList.add("warningsInput")
+    warningsPasswordCadastro.innerHTML = `⚠️Senha não pode ser vazia`;
 
-  // } else {
-  //   warningsPasswordCadastro.innerHTML = ""
-  //   repPasswordCadastro.classList.remove("warningsInput")
-  // }
-  // if (repPasswordCadastro.value.length < 6 || repPasswordCadastro.value.length > 20) {
-  //   repPasswordCadastro.classList.add("warningsInput")
-  //   warningsPasswordCadastro.innerHTML = `⚠️ Senha precisa ter no mínimo 6 e no máximo 10 carácteres`;
-  // } else {
-  //   warningsPasswordCadastro.innerHTML = ""
-  //   repPasswordCadastro.classList.remove("warningsInput")
-  // }
-  //  if (passwordCadastro.value != repPasswordCadastro.value) {
-  //   passwordCadastro.classList.add("warningsInput")
-  //   repPasswordCadastro.classList.add("warningsInput")
-  //   warningsPasswordCadastro.innerHTML = `⚠️ Senhas diferentes`;
-  //  } else {
-  //    warningsPasswordCadastro.innerHTML = ""
-  //    repPasswordCadastro.classList.remove("warningsInput")     
-  // }
+  } else {
+    warningsPasswordCadastro.innerHTML = ""
+    repPasswordCadastro.classList.remove("warningsInput")
+  }
+  if (repPasswordCadastro.value.length < 6 || repPasswordCadastro.value.length > 20) {
+    repPasswordCadastro.classList.add("warningsInput")
+    warningsPasswordCadastro.innerHTML = `⚠️ Senha precisa ter no mínimo 6 e no máximo 10 carácteres`;
+  } else {
+    warningsPasswordCadastro.innerHTML = ""
+    repPasswordCadastro.classList.remove("warningsInput")
+  }
+   if (passwordCadastro.value != repPasswordCadastro.value) {
+    passwordCadastro.classList.add("warningsInput")
+    repPasswordCadastro.classList.add("warningsInput")
+    warningsPasswordCadastro.innerHTML = `⚠️ Senhas diferentes`;
+   } else {
+     warningsPasswordCadastro.innerHTML = ""
+     repPasswordCadastro.classList.remove("warningsInput")     
+  }
   if (emailCadastro.value === null || emailCadastro.value == "") {
     emailCadastro.classList.add("warningsInput")
     warningsEmailCadastro.innerHTML = `⚠️ Email não pode ser vazio`;
@@ -140,11 +126,59 @@ const expresion = /\w+@\w+\.+[a-z]/;
     warningsEmailCadastro.innerHTML = ""
     emailCadastro.classList.remove("warningsInput")
   }
-  //warningsPasswordCadastro.innerHTML = msgErro.join(" -          ");
-  //document.getElementById("formCadastro").reset()
-  //document.getElementById('formCadastro').addEventListener('submit',formCadastro)
-  return false;
-})
+  await cadastroUser(event)
+  document.getElementById("formCadastro").reset()
 
-    
+}
 
+async function  envioLogin(event) {
+  event.preventDefault();
+
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+await fetch('http://localhost:8000/api/login', 
+  {
+    method: 'POST',
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify({email, password })
+    }).then(response => response.json())
+      .then(data => {
+      console.log(data);
+        window.localStorage.setItem('token', data.token);
+        window.localStorage.setItem('client', JSON.stringify(data))
+        if(data){
+          window.location.href = "http://127.0.0.1:5501/front-end/home.html"
+        } else {
+          window.location.href = "http://127.0.0.1:5501/index.html"
+        }
+    });
+}
+async function cadastroUser(event){
+event.preventDefault();
+
+const name = document.getElementById("nameCadastro").value;
+const email = document.getElementById("emailCadastro").value;
+const password = document.getElementById("passwordCadastro").value;
+    console.log(name, email, password);
+
+ await fetch('http://localhost:8000/api/register', 
+  {
+    method: 'POST',
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify({name, email, password })
+    }).then(response => response.json())
+      .then(data => {
+      console.log(data);
+        window.localStorage.setItem('token', data.token);
+        window.localStorage.setItem('client', JSON.stringify(data))
+      });
+}
